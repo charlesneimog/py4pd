eb564e4cea80c39e7daf1a92d12252de3d4893d3
Charles K. Neimog <charlesneimog@outlook.com>
2022-07-17 09:34:14 -0300

    add module to not quit python with Py_FinalizeEx().

--- py4pd.c
+++ py4pd.c
@@ -13,6 +13,8 @@ typedef struct _py { // It seems that all the objects are some kind of class.
     t_symbol        *x_pyscript; // symbol of the function
     t_float         *debug; // float value
     t_canvas        *x_canvas; // canvas
+    PyObject        *module; // python object
+    PyObject        *function; // function name
     t_outlet        *out_A; // outlet 1.
 }t_py;
 
@@ -29,7 +31,7 @@ static void py_set_function(t_py *x, t_symbol *s, int argc, t_atom *argv){
     PyObject *pName, *pModule, *pDict, *pFunc;
     PyObject *pArgs, *pValue;
 
-    if (argc < 3) {
+    if (argc < 2) {
         pd_error(x,"py4pd | run method | missing arguments");
         return;
     }
@@ -43,12 +45,85 @@ static void py_set_function(t_py *x, t_symbol *s, int argc, t_atom *argv){
     t_symbol *function_name = atom_gensym(argv+1);
         
     pName = PyUnicode_DecodeFSDefault(script_file_name->s_name); // Esse é o nome do script Python
-    pModule = PyImport_Import(pName); 
+    pModule = PyImport_Import(pName);
+    pFunc = PyObject_GetAttrString(pModule, function_name->s_name); // Esse é o nome da função Python 
     Py_DECREF(pName);
-    return;
+    if (pFunc && PyCallable_Check(pFunc)){ // Verifica se a função existe
+        // pFunc equal x_function
+        x->function = pFunc;
+        x->module = pModule;
+        post("py4pd | function %s loaded!", function_name->s_name);
+        return;
+    } else {
+        // post PyErr_Print() in pd
+        pd_error(x, "py4pd | function %s not loaded!", function_name->s_name);
+        post("\n===== PYTHON ERROR =====");
+        // show python error in pd using post
+        PyObject *ptype, *pvalue, *ptraceback;
+        PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+        PyErr_NormalizeException(&ptype, &pvalue, &ptraceback);
+        PyObject *pstr = PyObject_Str(pvalue);
+        post("%s", PyUnicode_AsUTF8(pstr));
+        post("========================\n");
+        return;
+    }
+}
+
+// ============================================
+static void py_run_without_quit_py(t_py *x, t_symbol *s, int argc, t_atom *argv){
+    PyObject *pName, *pModule, *pFunc; // pDict
+    PyObject *pArgs, *pValue;
+    pFunc = x->function;
+    pModule = x->module;
+    pArgs = PyTuple_New(argc);
+    int i;
+    for (i = 0; i < argc; ++i) {
+        if (argv[i].a_type == A_FLOAT) {
+            pValue = PyFloat_FromDouble(argv[i+0].a_w.w_float);
+            // post("py4pd | run method | float: %f", argv[i+0].a_w.w_float);
+        } else if (argv[i].a_type == A_SYMBOL) {
+            pValue = PyUnicode_DecodeFSDefault(argv[i].a_w.w_symbol->s_name);
+            // post("py4pd | run method | symbol: %s", argv[i].a_w.w_symbol->s_name);
+        } else {
+            pValue = Py_None;
+            Py_INCREF(Py_None);
+        }
+                
+        if (!pValue) {
+            // Py_DECREF(pArgs);
+            // Py_DECREF(pModule);
+            pd_error(x, "Cannot convert argument\n");
+            return;
+        }
+        PyTuple_SetItem(pArgs, i, pValue);
+    }
+
+    pValue = PyObject_CallObject(pFunc, pArgs);
+    
+    // Py_DECREF(pArgs);
+    
+    if (pValue != NULL) {
+        Py_DECREF(pValue);
+        outlet_float(x->out_A, PyLong_AsLong(pValue));
+    }
+    else {
+        PyErr_Print();
+        pd_error(x,"Call failed\n");
+        return;
+    }
 
 }
 
+
+// ============================================
+// ============================================
+// ============================================
+// ============================================
+// ============================================
+// ============================================
+// ============================================
+// ============================================
+// ============================================
 // ============================================
 
 static void py_run(t_py *x, t_symbol *s, int argc, t_atom *argv){
@@ -132,6 +207,9 @@ static void py_run(t_py *x, t_symbol *s, int argc, t_atom *argv){
 }
 
 // ============================================
+// ============================================
+// ============================================
+
 void *py_new(void){
     t_py *x = (t_py *)pd_new(py_class); // pointer para a classe
     x->out_A = outlet_new(&x->x_obj, &s_anything); // cria um outlet
@@ -139,11 +217,13 @@ void *py_new(void){
 }
 
 // ============================================
+// ============================================
+// ============================================
 
 void py4pd_free(t_py *x){
+    Py_FinalizeEx();
     outlet_free(x->out_A);
     pd_free((t_pd *)x);
-    Py_FinalizeEx();
 }
 
 // ====================================================
@@ -159,4 +239,5 @@ void py4pd_setup(void){
     class_addmethod(py_class, (t_method)py_run, gensym("run"), A_GIMME, 0); 
     class_addmethod(py_class, (t_method)py_home, gensym("home"), 0);
     class_addmethod(py_class, (t_method)py_set_function, gensym("set"), A_GIMME, 0);
+    class_addmethod(py_class, (t_method)py_run_without_quit_py, gensym("args"), A_GIMME, 0);
     }
