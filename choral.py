876344ebd51dfac29d156dec6dffaad63860feb5
Charles K. Neimog <charlesneimog@outlook.com>
2022-07-18 11:27:07 -0300

    update py script

--- choral.py
+++ choral.py
@@ -1,7 +1,16 @@
+import os
+import subprocess
+
+def delete_by_extension(ext):
+    folder = "C:/Users/Neimog/Git/py4pd"
+    test = os.listdir(folder)
+    for file in test:
+        if file.endswith(ext):
+            os.remove(folder + file)
+
 def cknnote(note, freq):
     import music21  
     from om_py.python_to_om import to_om
-    import os
     folder = "C:/Users/Neimog/Git/py4pd"
     us = music21.environment.UserSettings()
     us['lilypondPath'] = "C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe"
@@ -15,6 +24,6 @@ def cknnote(note, freq):
     s.insert(0, music21.dynamics.Dynamic('pp'))
     ly_file = s.write('lilypond', fp=folder_with_file_name)
     go_to_folder = 'cd "' + folder + '"'
-    cmd_line = go_to_folder + ' && "C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe" -s -fpng -dresolution=600 ' + str(ly_file)
-    os.system(cmd_line)
-    return ly_file
+    cmd_line = go_to_folder + ' && "C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe" -s -fpng -dresolution=1200 ' + str(ly_file)
+    subprocess.Popen(cmd_line, stdout=subprocess.PIPE, shell=True).stdout.read()
+    return 1
