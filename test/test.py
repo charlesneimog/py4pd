07f79aed773b77521de0788c4c8a49dc0e15eabc
INScore deployment <charlesneimog@outlook.com>
2023-02-09 17:52:34 -0300

    update path

--- test/test.py
+++ test/test.py
@@ -2,15 +2,15 @@ import os
 import sys
 
 if sys.platform == "win32":
-    os.chdir("../py4pd_WIN32")
+    os.chdir("../py4pd_WIN64")
     os.system('"C:\\Program Files\\Pure Data\\bin\\pd.exe" -nogui test.pd')
     
 elif sys.platform == "darwin":
     os.chdir("../py4pd_MACIntel")
-    os.system("/Applications/Pd-*.app/Contents/Resources/bin/pd -nogui test/test.pd")
+    os.system("/Applications/Pd-*.app/Contents/Resources/bin/pd -nogui test.pd")
     
 elif sys.platform == "linux2":
     os.chdir("../py4pd_LINUX")
-    os.system("pd -nogui test/test.pd")
+    os.system("pd -nogui test.pd")
               
 
