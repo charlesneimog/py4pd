e659d2a50db2b896d9b57f2c528819a9b352a8fe
INScore deployment <charlesneimog@outlook.com>
2023-03-05 21:04:11 -0300

    add scripts

---
+++ resources/scripts/pdpip.py
@@ -0,0 +1,15 @@
+import pd
+from pip._internal.cli.main import main as pipmain
+
+def py4pdInstall(package):
+    pd.print('Installing ' + package + ' , please wait...')
+    home = pd.home()
+    try:    
+        pipmain(['install', '--target', f'{home}/py-modules', package, '--upgrade'])    
+        pd.print('Installed ' + package + ' to ' + home + '/py-modules')
+    except Exception as e:
+        pd.error(str(e))
+
+
+
+
---
+++ resources/scripts/score.py
@@ -0,0 +1,4 @@
+import pd
+
+def test():
+    pd.print("External library working")
