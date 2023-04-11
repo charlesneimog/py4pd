d6b99ce6d112f8982181ff740c6695e4f89de220
INScore deployment <charlesneimog@outlook.com>
2023-04-11 16:11:59 -0300

    chore(pdpip.py): remove pdpip.py script from resources/scripts directory
    feat(py4pd.py): add pipinstall function to install Python packages from Pd
    feat(py4pd.py): add nth function to get the nth element of a list
    feat(py4pd.py): add omlist function to append a list to another list
    refactor(py4pd.py): remove pyiterate function as it is not used
    
    feat(py4pd.py): add new math functions: pymax, pymin, pyreduce. Add new list functions: nth, omlist. Add support for pip install.

--- resources/scripts/py4pd.py
+++ resources/scripts/py4pd.py
@@ -1,33 +1,144 @@
 import pd
+import os
+try:
+    from pip._internal.cli.main import main as pipmain
+    addpip = True
+except Exception as e:
+    pd.error("Error importing pip: " + str(e))
+    addpip = False
+
+
+def pipinstall(package):
+    """Install a Python package from Pd"""
+    if isinstance(package, list):
+        pd.print('Installing ' + package[1] + ' , please wait...')
+        folder = package[0]
+        if folder == "local":
+            folder = pd.home()
+
+        elif folder == "global":
+            folder = pd.py4pdfolder()
+
+        else:
+            pd.error("[py.install]: the first argument must be 'local' or 'global'")
+            return None
+        
+        package = package[1]
+    else:
+        pd.error("[py.install]: bad arguments")
+        return None
+    home = pd.home()
+    # If linux or macos
+    if os.name == 'posix':
+        try:
+            pipmain(['install', '--target', f'{folder}/resources/py-modules', package, '--upgrade'])
+            pd.print("Installed " + package)
+            return None
+        except Exception as e:
+            pd.error(str(e))
+            return None
+    
+    # If windows
+    elif os.name == 'nt':
+        import subprocess
+        try:
+            # change / to \\
+            home = home.replace('/', '\\')  
+            # install package from pip without opening a new window
+            subprocess.run(f'pip install --target {home}\\py-modules {package} --upgrade', shell=True, check=True)
+            pd.print("Installed " + package)
+            pd.print("I recommend restart PureData...")       
+        except Exception as e:
+            pd.error(str(e))
+            return None
+    else:
+        pd.error("Your OS is not supported")
+        return None
+
+# ================
+#  Info Functions
+#  ===============
 
 def pdprint(value):
     """Print a Pd Data type to the Pd console"""
     pd.print(str(value))
 
+#  =================
+# Convertion Objects
+#  =================
 
 def py2pd(value):
     """Convert a Python data type to a PureData type"""
     return value
 
-
 def pd2py(value):
     """Convert a PureData data type to a Python type"""
     return value
 
+#  ==============
+#  List Functions
+#  ==============
+
 def nth(list, n):
     """Get the nth element of a list"""
+    if n is None or list is None:
+        pd.error("[py.nth]: bad arguments")
+        return None
     return list[n]
 
-
-def pyiterate(value):
-    """Iterate over a Python data type"""
-    pd.iterate(value) 
+def omlist(*args):
+    """Append a list to another list"""
+    return list(args)
 
 
 def pylen(value):
     """Get the length of a Python data type"""
     return len(value)
 
+
+def pymax(value):
+    """Get the maximum value of a Python data type"""
+    return max(value)
+
+
+def pymin(value):
+    """Get the minimum value of a Python data type"""
+    return min(value)
+
+
+def pyreduce(key, value):
+    """Reduce a Python data type"""
+    if value == [] or value == None:
+        pd.error("[py.reduce]: bad arguments")
+        return None
+
+    if key == "+":  
+        return sum(value)
+    elif key == "-":
+        result = 0
+        for i in value:
+            result = result - i
+        return result
+    elif key == "*":
+        result = 1
+        for i in value:
+            result = result * i
+        return result
+    elif key == "/":
+        result = 1
+        for i in value:
+            result = result / i
+        return result
+    else:
+        pd.error("[py.reduce]: bad arguments")
+
+#  ==============
+#  Loop Functions
+#  ==============
+
+def pyiterate(value):
+    """Iterate over a Python data type"""
+    pd.iterate(value) 
 
 def pycollect(data):
     pointer = pd.getobjpointer()
@@ -51,6 +162,9 @@ def pycollect(data):
         else:
             globals()[string] = [data]  
 
+#  ==============
+#  Math Functions
+#  ==============
 
 def omsum(a, b):
     """ Sum two numbers or two lists of numbers """
@@ -123,22 +237,34 @@ def omabs(a):
 # =============================================
 
 def py4pdLoadObjects():
+    # Pip install
+    if addpip:
+        pd.addobject(pipinstall, "py.pip")
+
+    # info
     pd.addobject(pdprint, "py.print", no_outlet=True)
+    
+    # Convertion Objects
     pd.addobject(py2pd, "py2pd")
     pd.addobject(pd2py, "pd2py", pyout=True)
 
-    # loop functions
-    pd.addobject(pyiterate, "py.iterate")
-    pd.addobject(pycollect, "py.collect")
-
-    # list functions
+    # List Functions
     pd.addobject(pylen, "py.len")
-    pd.addobject(nth, "py.nth")
+    pd.addobject(nth, "py.nth", pyout=True)
+    pd.addobject(omlist, "py.list", pyout=True)
+    pd.addobject(pymax, "py.max")
+    pd.addobject(pymin, "py.min")
+    pd.addobject(pyreduce, "py.reduce", pyout=True)
 
-    # ordinary functions
+    # Loop Functions
+    pd.addobject(pyiterate, "py.iterate") # these are special objects, they don't have a pyout argument but output py data types
+    pd.addobject(pycollect, "py.collect") # these are special objects, they don't have a pyout argument but output py data types
+
+    # Math Functions
     pd.addobject(omsum, "py.sum")
     pd.addobject(omminus, "py.minus")
     pd.addobject(omtimes, "py.times")
     pd.addobject(omdiv, "py.div")
     pd.addobject(omabs, "py.abs")
+
 
