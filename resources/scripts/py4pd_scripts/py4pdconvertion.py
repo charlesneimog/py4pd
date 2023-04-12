d8cfdd271856b04383b57daad39a0e47eaf8b89d
INScore deployment <charlesneimog@outlook.com>
2023-04-12 18:34:48 -0300

    refactor(py4pd.py): remove unused code and import py4pd scripts modules feat(py4pd.py): add py4pd scripts modules to py4pdLoadObjects function
    feat(py4pd.py): add mat_trans object to py4pdLoadObjects() function
    
    feat(py4pdconvertion.py): add conversion functions py2pd and pd2py for Python and PureData data types respectively
    docs(py4pdconvertion.py): add comments to conversion functions and loop functions

---
+++ resources/scripts/py4pd_scripts/py4pdconvertion.py
@@ -0,0 +1,15 @@
+
+#  =================
+# Convertion Objects
+#  =================
+
+def py2pd(value):
+    """Convert a Python data type to a PureData type"""
+    return value
+
+def pd2py(value):
+    """Convert a PureData data type to a Python type"""
+    return value
+#  ==============
+#  Loop Functions
+#  ==============
