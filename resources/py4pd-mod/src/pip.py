d8a26b9b7aa107a97cd628c1a1d7519a58a49504
Charles K. Neimog <charlesneimog@outlook.com>
2023-11-29 18:14:09 -0300

    try to update pip for mac os

--- resources/py4pd-mod/src/pip.py
+++ resources/py4pd-mod/src/pip.py
@@ -12,51 +12,6 @@ package = ""
 folder = ""
 
 
-class MacOSpip:
-    def __init__(self, pippackage, pipfolder):
-        import tkinter as tk
-
-        self.package = pippackage
-        self.folder = pipfolder
-        self.window = tk.Tk()
-        # icon_file = pd.py4pdfolder() + "/resources/icons/pd.icns"
-        # self.window.iconbitmap(icon_file)
-        # self.window.protocol("WM_DELETE_WINDOW", self.close_window)
-        self._pipinstall()  # Renamed to avoid naming conflict
-
-    def drawWarning(self):
-        from tkinter import Label, LabelFrame, Tk
-
-        # get screen width and height
-        root = self.window
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-
-        # calculate x and y coordinates for the Tk root window to center it on the screen
-        x = (screen_width / 2) - (300 / 2)
-        y = (screen_height / 2) - (100 / 2)
-
-        root.geometry("300x100+%d+%d" % (x, y))
-        root.resizable(False, False)
-
-        # create text
-        text = LabelFrame(
-            root, text="Installing " + package + " , please wait...", padx=20, pady=20
-        )
-        text.pack(fill="both", expand=1)
-
-        # add label inside the label frame
-        label = Label(
-            text,
-            text="Installing " + package + " , please wait...",
-            anchor="center",
-            justify="center",
-        )
-        label.pack(fill="both", expand=1)
-
-        # update window
-        self.window.update()
-
     def _pipinstall(self):
         self.drawWarning()
         version = sys.version_info
@@ -218,9 +173,27 @@ def pipinstall(mypackage):
 
         elif platform.system() == "Darwin":
             try:
-                my_window = MacOSpip(package, folder)
-                my_window.run()
-
+                value = subprocess.run(
+                    [
+                        f"/usr/local/bin/python{major}.{minor}",
+                        "-m",
+                        "pip",
+                        "install",
+                        "--target",
+                        f"{folder}/py-modules",
+                        package,
+                        "--upgrade",
+                    ],
+                    check=True,
+                )
+                if value.returncode != 0:
+                    pd.logpost(3, "pip return value" + str(value))
+                    pd.error(
+                        "You need to restart PureData, to check if the installation process worked"
+                    )
+                else:
+                    pd.print(f"{package} Installed!")
+                    pd.error("You need to restart PureData")
             except Exception as e:
                 pd.error(str(e))
         else:
