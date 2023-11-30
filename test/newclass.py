76cc00c4aa516d4427b5512bbbf74bc1b6e48dbb
Charles K. Neimog <charlesneimog@outlook.com>
2023-11-30 10:34:40 -0300

    test updates

---
+++ test/newclass.py
@@ -0,0 +1,16 @@
+import pd
+
+
+pd.print(pd.add_object())
+
+
+
+def sumlist(x, valuetimes):
+    mylist = []
+    for i in x:
+        print(i)
+        mylist.append(i * valuetimes)
+    return mylist
+    
+
+    
