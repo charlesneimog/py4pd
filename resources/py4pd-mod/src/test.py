3e6dffd2c56abea6befb43426d547c563dc85577
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-01 12:55:48 -0800

    fix memuse for mac os

--- resources/py4pd-mod/src/test.py
+++ resources/py4pd-mod/src/test.py
@@ -3,7 +3,6 @@ import time
 import subprocess
 import platform
 
-
 def py4pdtimer(message):
     if (message == "start"):
         pd.set_obj_var("py4pd_timer", time.time())
@@ -14,25 +13,29 @@ def py4pdtimer(message):
         pd.error("Bad args to py4pdtimer")
 
 def getMemoryUse(programName):
-    # Check if the platform is Linux
     if platform.system() == 'Linux':
         try:
-            # Execute pidof to get the pid of the program using programName
             pid = subprocess.check_output(["pidof", programName])
-            # Convert the byte string to a string and remove the newline character
             pid = pid.decode("utf-8").strip()
             if isinstance(pid, list):
                 pd.error("More than one pid found for " + programName)
                 return 0    
             memoryUse = int(subprocess.check_output(["ps", "-o", "rss=", pid]).strip())
             memoryUse = int(memoryUse / 1024)
-            # Return the memory usage
             return memoryUse
         except subprocess.CalledProcessError as e:
             pd.error(f"Error retrieving memory usage: {e}")
             return 0
+    elif platform.system() == 'Darwin':
+        try:
+            command = 'top -l 1 -stats pid,command,cpu,mem | grep Pd'
+            result = subprocess.check_output(command, shell=True, text=True)
+            result = result.split()[3]
+            result = result[:-1]           
+            return result
+        except subprocess.CalledProcessError:
+            return None
     else:
-        # Return 1 for non-Linux platforms (Mac and Windows)
         return 1
 
 
