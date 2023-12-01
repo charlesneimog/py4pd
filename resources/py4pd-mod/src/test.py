92aff46795147fd0be620e97d8b2856c3b426985
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-01 11:54:51 -0800

    fix function getMemoryUse for no Linux OSs

--- resources/py4pd-mod/src/test.py
+++ resources/py4pd-mod/src/test.py
@@ -1,6 +1,7 @@
 import pd
 import time
 import subprocess
+import platform
 
 
 def py4pdtimer(message):
@@ -12,22 +13,26 @@ def py4pdtimer(message):
     else:
         pd.error("Bad args to py4pdtimer")
 
-
-
-
-
 def getMemoryUse(programName):
-    # execute pidof to get the pid of the program using programName
-    pid = subprocess.check_output(["pidof", programName])
-    # convert the byte string to a string and remove the newline character
-    pid = pid.decode("utf-8").strip()
-    if isinstance(pid, list):
-        pd.error("More than one pid found for " + programName)
-        return 0    
-    memoryUse = int(subprocess.check_output(["ps", "-o", "rss=", pid]).strip())
-    memoryUse = int(memoryUse / 1024)
-    # return the memory usage
-    return memoryUse
-
+    # Check if the platform is Linux
+    if platform.system() == 'Linux':
+        try:
+            # Execute pidof to get the pid of the program using programName
+            pid = subprocess.check_output(["pidof", programName])
+            # Convert the byte string to a string and remove the newline character
+            pid = pid.decode("utf-8").strip()
+            if isinstance(pid, list):
+                pd.error("More than one pid found for " + programName)
+                return 0    
+            memoryUse = int(subprocess.check_output(["ps", "-o", "rss=", pid]).strip())
+            memoryUse = int(memoryUse / 1024)
+            # Return the memory usage
+            return memoryUse
+        except subprocess.CalledProcessError as e:
+            pd.error(f"Error retrieving memory usage: {e}")
+            return 0
+    else:
+        # Return 1 for non-Linux platforms (Mac and Windows)
+        return 1
 
 
