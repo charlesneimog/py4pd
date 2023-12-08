fbbc45691b570686d93fdb4d334f332620fefa8c
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-08 18:24:32 -0300

    support mac os in memuse

--- resources/py4pd-mod/src/test.py
+++ resources/py4pd-mod/src/test.py
@@ -1,41 +1,42 @@
-import pd
-import time
-import subprocess
 import platform
+import subprocess
+import time
+
+import pd
+
 
 def py4pdtimer(message):
-    if (message == "start"):
+    if message == "start":
         pd.set_obj_var("py4pd_timer", time.time())
-    elif (message == "end"):
+    elif message == "end":
         timer = time.time() - pd.get_obj_var("py4pd_timer")
         pd.print("It spent " + str(int(timer * 1000)) + " ms.")
     else:
         pd.error("Bad args to py4pdtimer")
 
-def getMemoryUse(programName):
-    if platform.system() == 'Linux':
+
+def getMemoryUse():
+    if platform.system() == "Linux":
         try:
-            pid = subprocess.check_output(["pidof", programName])
+            pid = subprocess.check_output(["pidof", "pd"])
             pid = pid.decode("utf-8").strip()
             if isinstance(pid, list):
-                pd.error("More than one pid found for " + programName)
-                return 0    
+                pd.error("More than one pid found for " + "pd")
+                return 0
             memoryUse = int(subprocess.check_output(["ps", "-o", "rss=", pid]).strip())
             memoryUse = int(memoryUse / 1024)
             return memoryUse
         except subprocess.CalledProcessError as e:
             pd.error(f"Error retrieving memory usage: {e}")
             return 0
-    elif platform.system() == 'Darwin':
+    elif platform.system() == "Darwin":
         try:
-            command = 'top -l 1 -stats pid,command,cpu,mem | grep Pd'
+            command = "top -l 1 -stats pid,command,cpu,mem | grep Pd"
             result = subprocess.check_output(command, shell=True, text=True)
             result = result.split()[3]
-            result = result[:-1]           
+            result = result[:-1]
             return result
         except subprocess.CalledProcessError:
             return None
     else:
         return 1
-
-
