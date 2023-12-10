cc257887e43928f9fc4fd9e33799d759fa412fab
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-10 15:47:27 -0300

    test updates

--- test/py.py
+++ test/py.py
@@ -5,12 +5,8 @@ import pd
 
 try:
     import numpy as np
-
     numpyIsInstalled = True
 except Exception as e:
-    import py4pd
-    py4pd.pipinstall(["local", "numpy"])
-    numpyIsInstalled = False
     pd.error("You must restart Pure Data to use numpy.")
     sys.exit()
 
@@ -54,6 +50,13 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+
+# ================================================
+# ================== LISTS  ======================
+# ================================================
+
+def randomNumpyArray():
+    return np.random.rand(1, 64).tolist()[0]
 
 # ================================================
 # ================== AUDIO  ======================
@@ -126,6 +129,7 @@ def printall(x, y):
 # ================================================
 # ================ Audio =========================
 # ================================================
+
 def generate_sine_wave(frequency, amplitude, phase, num_samples, sampling_rate):
     angular_frequency = 2 * np.pi * frequency
     t = np.arange(num_samples) / sampling_rate
@@ -137,36 +141,59 @@ def generate_sine_wave(frequency, amplitude, phase, num_samples, sampling_rate):
 def mksenoide(freqs, amps, phases, vectorsize, samplerate):
     n = len(freqs)
     nframes = vectorsize
-    out = np.zeros(
-        (n, nframes), dtype=np.float64
-    )  # Modify the shape of the output array
-    new_phases = np.zeros(n, dtype=np.float64)  # Array to store the new phases
+    out = np.zeros((n, nframes))  # Array with 2 dimensions: channels by frames
+    new_phases = np.zeros(n)  # Array to store the new phases
     for i in range(n):
         out[i], new_phases[i] = generate_sine_wave(
             freqs[i], amps[i], phases[i], nframes, samplerate
         )
-        if new_phases[i] > 2 * np.pi:
-            new_phases[i] -= 2 * np.pi
     return out, new_phases
 
 
 def sinusoids(freqs, amps):
-    vectorsize = pd.get_vec_size()
-    samplerate = pd.get_sample_rate()
     if freqs is None or amps is None:
-        return None
+        pd.error("You need to set the amplitudes")
+        return np.zeros((1, 64))
     if len(freqs) != len(amps):
+        pd.error("The length of freqs and amps are different")
         return None
+
     phases = pd.get_obj_var("PHASE", initial_value=np.zeros(len(freqs)))
     freqs = np.array(freqs, dtype=np.float64)
     amps = np.array(amps, dtype=np.float64)
-    out, new_phases = mksenoide(freqs, amps, phases, vectorsize, samplerate)
+    out, new_phases = mksenoide(freqs, amps, phases, 64, pd.get_sample_rate())
     pd.set_obj_var("PHASE", new_phases)
     return out
 
 
 def audioin(audio):
-    return 0
+    num_channels = audio.shape[0]
+    fftresult = []
+    for channel in range(num_channels):
+        channel_data = audio[channel, :]
+        fft_result = np.fft.fft(channel_data)
+        first_real_number = np.real(fft_result[0])
+        fftresult.append(first_real_number)
+    return fftresult
+
+
+def generate_audio_noise():
+    noise_array = np.random.rand(5, 64)
+    return noise_array
+
+
+def audioInOut(audio):
+    num_channels = audio.shape[0]
+    transformed_audio = np.empty_like(audio, dtype=np.float64)
+
+    for channel in range(num_channels):
+        channel_data = audio[channel, :]
+        fft_result = np.fft.fft(channel_data)
+        ifft_result = np.fft.ifft(fft_result).real
+        transformed_audio[channel, :] = ifft_result
+
+    return transformed_audio
+
 
 
 
@@ -178,3 +205,7 @@ def audioin(audio):
 def py4pdLoadObjects():
     pd.add_object(sinusoids, "sinusoids~", objtype=pd.AUDIOOUT)
     pd.add_object(audioin, "audioin~", objtype=pd.AUDIOIN)
+    pd.add_object(audioInOut, "audio~", objtype=pd.AUDIO)
+    pd.add_object(generate_audio_noise, "pynoise~", objtype=pd.AUDIOOUT)
+    pd.add_object(randomNumpyArray, "random-array", pyout=True)
+    
