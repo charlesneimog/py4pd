0c4153ae96ec9ec4c9f8b15bd650a0f40872127a
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-11 23:37:51 -0300

    initial tests

---
+++ test/not-work-yet/threadtest.py
@@ -0,0 +1,18 @@
+import time
+import pd
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+    
+    
+def sum_of_primes(start, end):
+    return sum(num for num in range(start, end + 1) if is_prime(num))  
+    
+def mytest():
+    return sum_of_primes(1, 1000000)
