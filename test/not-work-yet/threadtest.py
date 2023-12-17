aa468416d18ea15522aa58966de084704a025872
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-17 19:24:43 -0300

    add patches releated with python3.12 and ext-class.c

--- test/not-work-yet/threadtest.py
+++ test/not-work-yet/threadtest.py
@@ -1,3 +1,7 @@
+import numpy
+import pd
+
+
 def is_prime(num):
     if num < 2:
         return False
@@ -5,11 +9,49 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
-    
-    
+
+
+def exe():
+    import sys
+
+    python_executable_path = sys.executable
+    return python_executable_path
+
+
 def sum_of_primes(start, end):
-    return sum(num for num in range(start, end + 1) if is_prime(num))  
+    return sum(num for num in range(start, end + 1) if is_prime(num))
+
+def floatmethod():
+    pd.print("Hello from float method")
+
+def symbolmethod():
+    pd.print("Hello from symbol method")
+
+def listmethod():
+    pd.print("Hello from list method")
     
+def anythingmethod():
+    pd.print("Hello from anything method")   
+    
+def bangmethod():
+    pd.print("Hello from bang method")
+    
+def selectormethod():
+    pd.print("Hello from selector method")
+    
+def myclass():
+    newobj = pd.new_object("myclass")
+    newobj.addmethod_float(floatmethod)
+    newobj.addmethod_symbol(symbolmethod)
+    newobj.addmethod_list(listmethod)
+    newobj.addmethod_anything(anythingmethod)
+    newobj.addmethod_bang(bangmethod)
+    newobj.addmethod("mymethod", selectormethod)
+    newobj.addmethod("mymethod2", selectormethod, arg_types=(pd.A_FLOAT, pd.A_SYMBOL)) 
+    newobj.add_object()
+
+
+
 def mytest():
     sum_of_primes(1, 1000000)
     return "ok"
