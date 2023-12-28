276cebe14194480467cbc60836f377578e8ccec0
Charles K. Neimog <charlesneimog@outlook.com>
2023-12-27 22:35:58 -0300

    minor docs fixes

--- docs/examples/ia/ia.py
+++ docs/examples/ia/ia.py
@@ -1,16 +1,19 @@
 import pd
-for library in ['torch', 'librosa']:
+
+for library in ["torch", "librosa"]:
     try:
-        exec('import ' + library)
+        exec("import " + library)
     except:
-        pd.error('Please install ' + library + ' library')
+        pd.error("Please install " + library + " library")
         import sys
+
         sys.exit(1)
-        
+
 
 def renderAudio_nn(audio, model):
-    model = pd.get_patch_dir() + '/' + model
-    audio = pd.get_patch_dir() + '/' +  audio
+    pd.print(audio)
+    model = pd.get_patch_dir() + "/" + model
+    audio = pd.get_patch_dir() + "/" + audio
     torch.set_grad_enabled(False)
     model = torch.jit.load(model).eval()
     x = librosa.load(audio)[0]
@@ -18,11 +21,11 @@ def renderAudio_nn(audio, model):
     z = model.encode(x_for)
     z[:, 0] += torch.linspace(-2, 2, z.shape[-1])
     y = model.decode(z).numpy().reshape(-1)
-    pd.tabwrite('iaAudio', y.tolist(), resize=True)
-    pd.print('Audio rendered')
-
+    # split y in half
+    y = y[: int(len(y) / 2)]
+    pd.tabwrite("iaAudio", y, resize=True)
+    pd.print("Audio rendered")
 
 
 def py4pdLoadObjects():
     pd.add_object(renderAudio_nn, "py4pd-nn")
-
