7bad030e26577dba17f8522ff66dbfcf14375105
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-14 10:59:16 -0300

    rename folder again

--- docs/examples/new_object/anything.py
+++
@@ -1,9 +0,0 @@
-import pd
-
-def anythingmethod(mything):
-    return f"Received {mything}"
-
-def py4pdLoadObjects():
-    random = pd.new_object("py.anything")
-    random.addmethod_anything(anythingmethod)
-    random.add_object()
