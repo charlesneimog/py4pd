ecbce40e8bc8acae1352b49c0bfbd9788771520c
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-15 16:33:45 -0300

    see how easy is to work with pybind11

--- Sources/py4pd/openmusic.py
+++
@@ -1,8 +0,0 @@
-import pd
-
-
-def arithm_ser(start, stop, step):
-    list = []
-    for i in range(start, stop, step):
-        list.append(i)
-    return list
--- Sources/py4pd/show.py
+++
@@ -1,9 +0,0 @@
-import pd
-import os
-
-def py4pdshow(img):
-    if not os.path.exists(img):
-        img = os.path.join(pd.get_patch_dir(), img)
-    
-    pd.show_image(img)
-
